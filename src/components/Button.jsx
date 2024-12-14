f35cb7abcebd71c486c53db4d5e1c36a4c502b10
Rayan-360 <skrayan0017@gmail.com>
2024-12-14 18:29:03 +0530

    added components services,popularProducts,specialOffer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Button = (props) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full bg-coral-red text-white border-coral-red">
-        {props.label}<img src={props.iconURL} alt="arrow-right icon" className="ml-2 rounded-full bg-white w-5 h-5"/>
+        {props.label}{ props.iconURL && <img src={props.iconURL} 
+                          alt="arrow-right icon" 
+                          className="ml-2 rounded-full bg-white w-5 h-5"/>}
     </button>
   )
 }
