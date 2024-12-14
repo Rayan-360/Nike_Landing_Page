f2c5b9de145df9d7025e6b87010813f614c6a4d7
Rayan-360 <skrayan0017@gmail.com>
2024-12-14 15:12:16 +0530

    Completed hero component

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    SpecialOffer,
+    Footer,
+    Subscribe,
+    SuperQuality,
+    Services
+}
